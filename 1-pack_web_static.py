8fbd5b4c5a3c24a92829080e1decf48b3e34d2f6
Joseph50079 <josephogbah2019@gmail.com>
2024-07-12 06:06:43 +0100

    file tar

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 def do_pack():
     now = datetime.now.strftime("%Y%m%d%H%M%S")
-    file = web_static_f"{now}".tgz
+    file = "web_static_{}.tgz".format(now)
     local('mkdir -p versions')
     correct = local('tar -czvf versions/{}".tgz web_static'.format(file))
     if correct is not None:
