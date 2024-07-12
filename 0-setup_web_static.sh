2d4acaf3e9d52ded635c0110ed2fae0edc795972
Joseph50079 <josephogbah2019@gmail.com>
2024-07-12 01:48:49 +0100

    seting up web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+mkdir -p /data/web_static/shared
+mkdir -p "data/web_static/releases/test"
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+rm -rf /data/web_static/current
+touch /data/web_static/current
+ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu data
+printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   /var/www/html;
+    index  index.html index.htm;
+    
+    location /hbnb_static { 
+    	add_header X-Served-By "$HOSTNAME";
+	alias /data/web_static/current/;
+    }
+}" >> /etc/nginx/sites-available/default
+service nginx restart
+
