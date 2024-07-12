eb59fbc680e9c6cde4280bcec45b02401584b155
Joseph50079 <josephogbah2019@gmail.com>
2024-07-12 05:15:54 +0100

    doing

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,8 +2,8 @@
 
 """A fabric file that archive and send's it to remote server"""
 
-from fabric import task
+from fabric import *
 
-@task
+
 def do_pack():
     local('tar -czvf versions/web_static_"$(date +"%Y%m%d%H%M%S")".tgz web_static')
