81048da940d7fb050bb628fe723502d11437510f
Joseph50079 <josephogbah2019@gmail.com>
2024-07-16 00:38:04 +0100

    try

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,6 +3,8 @@
 """Module for deloying local files to the server using Fabric API"""
 import os
 from fabric.api import *
+env.hosts = ['52.201.156.75', '54.172.255.78']
+env.user = 'ubuntu'
 
 
 def do_pack():
@@ -22,16 +24,24 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    env.hosts = ['52.201.156.75', '54.172.255.78']
-    env.user = 'ubuntu'
+    """
+    Deploys archived file to server
+
+    Args:
+        archive_path: path to the archived file
+     
+    """
+
     if os.path.exists(archive_path):
         return False
-    else:
-        put(archive_path, remote='/tmp/')
-        web_dir = archive_path.split('/')[1].split('.')[0]
+    try:
+        put(archive_path, '/tmp/')
+        web_dir = archive_path.split('/')[-1].split('.')[0]
         run('mkdir -p /data/web_static/releases/{}'.format(web_dir))
         run('tar -xvzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_path, web_dir))
         run('rm -rf /tmp/{}'.format(archive_path))
         run('rm -rf /data/web_static/current')
-        run('ln -sF /data/web_static/releases/{} /data/web_static/current')
+        run('ln -sF /data/web_static/releases/{} /data/web_static/current'.format(web_dir)
         return True
+    except:
+        return False
