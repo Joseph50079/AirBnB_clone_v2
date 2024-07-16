acd0c0363d28b187e0be1c01cdcd6a394aab09e4
Joseph50079 <josephogbah2019@gmail.com>
2024-07-16 01:22:25 +0100

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -45,6 +45,7 @@ def do_deploy(archive_path):
         web_dir = archive_path.split('/')[-1].split('.')[0]
         run('mkdir -p /data/web_static/releases/{}'.format(web_dir))
         run('tar -xvzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_file, web_dir))
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(web_dir))
         run('rm -rf /tmp/{}'.format(archive_file))
         run('rm -rf /data/web_static/current')
         run('ln -sF /data/web_static/releases/{} /data/web_static/current'.format(web_dir))
