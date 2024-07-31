9b1b85fc6c2340d55a8a3dbd021b7513926b4d3d
Joseph50079 <josephogbah2019@gmail.com>
2024-07-31 23:03:13 +0100

    flask route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""flask routing module"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """home route for home page or root page"""
+
+    return "Hello, HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
