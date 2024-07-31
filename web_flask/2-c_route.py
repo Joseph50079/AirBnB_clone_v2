57bfdfc7003e89d18ac3465bc2c820fb3840d1a2
Joseph50079 <josephogbah2019@gmail.com>
2024-08-01 00:15:27 +0100

    markupsafe

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""flask routing module"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """home route for home page or root page"""
+
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb route for hbnb page"""
+
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb():
+    """hbnb route for hbnb page"""
+
+    return f"C {escape(text)}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
