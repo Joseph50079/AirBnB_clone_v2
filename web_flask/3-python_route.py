955fb39467a335dd0f8e4c3a693cb38346274eb4
Joseph50079 <josephogbah2019@gmail.com>
2024-08-01 01:25:54 +0100

    ptyhonflask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""flask routing module"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """home route for home page or root page"""
+
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb route for hbnb page"""
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_text(text):
+    """C_text route for dynamic text page"""
+
+    return f"C {escape(text).replace('_', ' ')}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """python_text route for dynamic text page"""
+
+    return f"Python {escape(text).replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
